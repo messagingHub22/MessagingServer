e574a98006e4490c07259bde8f12430cc5b9c71d
YAJATapps <yajatcomputer@gmail.com>
2022-09-19 17:12:56 -0700

    Add signalR hub

---
+++ MessagingServer/Hubs/MessagingHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MessagingServer.Hubs
+{
+    public class MessagingHub : Hub
+    {
+        public async Task ReloadMessage(string user)
+        {
+            await Clients.All.SendAsync("ReloadMessage", user);
+        }
+    }
+}
--- MessagingServer/Program.cs
+++ MessagingServer/Program.cs
@@ -1,3 +1,5 @@
+using MessagingServer.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -21,5 +25,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<MessagingHub>("/messagingHub");
 
 app.Run();

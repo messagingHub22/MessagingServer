e858727b6b8f0dc17e4ffb11dfc56c765aea8387
Yajat Kumar <yajatapps3@gmail.com>
2022-10-28 11:42:14 -0700

    Start adding login

--- MessagingServer/Hubs/MessagingHub.cs
+++ MessagingServer/Hubs/MessagingHub.cs
@@ -4,9 +4,28 @@ namespace MessagingServer.Hubs
 {
     public class MessagingHub : Hub
     {
+        private string userConnected;
+
+        public string UserConnected { get => userConnected; set => userConnected = value; }
+
+        // Sends the updated user to all connected clients
         public async Task ReloadMessage(string user)
         {
             await Clients.All.SendAsync("ReloadMessageClient", user);
         }
+
+        // Sends a call to the particular user who got new message
+        public async Task ReloadMessageForUser(string user)
+        {
+            // TODO
+            await Clients.Client("").SendAsync("ReloadMessageClient", user);
+        }
+
+        // Logs in the user
+        public async Task LoginUser(string user)
+        {
+           userConnected = user;
+        }
+        
     }
 }

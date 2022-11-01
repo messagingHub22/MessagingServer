8b6aff094bf0cdbd5b0b5ee77d80faebfa84c261
Yajat Kumar <yajatapps3@gmail.com>
2022-11-01 16:24:45 -0700

    Method to send update only to user who got message

--- MessagingServer/Hubs/MessagingHub.cs
+++ MessagingServer/Hubs/MessagingHub.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Xml.Linq;
 
 namespace MessagingServer.Hubs
 {
     public class MessagingHub : Hub
     {
-        private string userConnected;
-
-        public string UserConnected { get => userConnected; set => userConnected = value; }
+        // Map usernames to connection ids
+        private readonly static ConnectionMapping<string> _connections =
+            new ConnectionMapping<string>();
 
         // Sends the updated user to all connected clients
         public async Task ReloadMessage(string user)
@@ -17,15 +18,35 @@ namespace MessagingServer.Hubs
         // Sends a call to the particular user who got new message
         public async Task ReloadMessageForUser(string user)
         {
-            // TODO
-            await Clients.Client("").SendAsync("ReloadMessageClient", user);
+            foreach (var connectionId in _connections.GetConnections(user))
+            {
+                await Clients.Client(connectionId).SendAsync("ReloadClientUser");
+            }
         }
 
-        // Logs in the user
-        public async Task LoginUser(string user)
+        public override Task OnConnectedAsync()
         {
-           userConnected = user;
+            string name = Context.GetHttpContext().Request.Query["username"];
+
+            if (name != null) // Name is null when client is server page
+            {
+                _connections.Add(name, Context.ConnectionId);
+            }
+
+            return base.OnConnectedAsync();
         }
-        
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            string name = Context.GetHttpContext().Request.Query["username"];
+
+            if (name != null)
+            {
+                _connections.Remove(name, Context.ConnectionId);
+            }
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
     }
 }

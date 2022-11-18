cd4a5d2ea4feabe89e5ca867f382d6dc603e3012
Yajat Kumar <yajatapps3@gmail.com>
2022-11-17 18:35:12 -0800

    Add a unit test

---
+++ MessagingServerTests/Controllers/MessageDataControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MessagingServer.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessagingServer.Data;
+
+namespace MessagingServer.Controllers.Tests
+{
+    [TestClass()]
+    public class MessageDataControllerTests
+    {
+        [TestMethod()]
+        public void GetMessagesTest()
+        {
+            MessageDataController controller = new MessageDataController(null);
+            IEnumerable<MessageData> messages;
+
+            // Task.Delay(5000).ContinueWith(t => Assert.Fail("Did not respond in 5 seconds. Database might be connecting."));
+
+            try
+            {
+                messages = controller.GetMessages();
+            } catch
+            {
+                Assert.Fail("Failed to initialize database. Check connection string.");
+            }
+
+        }
+
+
+    }
+}
